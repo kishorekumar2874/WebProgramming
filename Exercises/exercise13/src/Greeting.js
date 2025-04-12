cf7d03ca20ecf1c64cc924bae0a2a8468e42a131
kishorekumar2874 <kishorekumar.m2023@vitstudent.ac.in>
2025-04-13 00:17:46 +0530

    Delete Exercises/exercise13 directory

--- Exercises/exercise13/src/Greeting.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Greeting = ({ isMorning }) => {
-  return <h1>{isMorning ? "Good Morning" : "Good Evening"}</h1>;
-};
-export default Greeting;
--- Exercises/exercise13/src/StyledMessage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const StyledMessage = () => {
-  const style = { color: "blue", fontSize: "20px", fontWeight: "bold" };
-  return <p style={style}>This is a styled message!</p>;
-};
-export default StyledMessage;
